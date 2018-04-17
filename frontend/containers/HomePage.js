fb3b67032f149714509308f1b220d94e5dce727b
Nicholas Raphael <n_raphael@live.com>
2018-04-17 00:52:56 -0400

    Added homepage copy

--- frontend/containers/HomePage.js
+++ frontend/containers/HomePage.js
@@ -17,33 +17,21 @@ class HomePage extends React.Component {
 
         <div className="header-1">
           <Header as='h1' floated='right'>
-            What is The Erudite Child?
+            The Erudite Child
           </Header>
         </div>
         <div className="home-info-1">
           <Grid>
             <Grid.Column width={4}>
-              <Image src='/assets/images/image.png' />
-              <Image src="https://water.org/media/images/Waterorg_Homepage_Img-5.original.jpg" />
+              <Image src='/assets/images/child.png' size="small"/>
             </Grid.Column>
             <Grid.Column width={9}>
                   <div className="home-text-1">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Headquarted and active in India the Erudite Child
+                        is an organization dedicated to providing better facilites and education
+                        to a number of children in communities across the country.
                     </p>
-
-
-        <a className="twitter-grid" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets</a>
-        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-        <script id="twitter-wjs" type="text/javascript" async defer src="//platform.twitter.com/widgets.js"></script>
-        <a className="twitter-timeline" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-
                   </div>
             </Grid.Column>
           </Grid>
@@ -70,13 +58,15 @@ class HomePage extends React.Component {
              </Grid.Column>
              <div className="home-text-2">
                <p>
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                   nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                   in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                   Excepteur sint occaecat cupidatat non proident,
-                   sunt in culpa qui officia deserunt mollit anim id est laborum.
+                   One of the major barriers to education for most financing.
+                   Through our organization you can help hundreds of children gain
+                   access to a better education. You can donate whenever you want and
+                   also choose to engage every month with our Adopt A Child initiative which allows
+                   you to donate monthly to a child in need that you can connect with.
+               </p>
+               <p>
+                   By supporting The Erudite Child, you are part of a solution that reaches children in need.
+                   So far, we have reached dozens - and our goal is to grow and impact millions and you can help us.
                </p>
              </div>
           </Grid>
@@ -88,14 +78,11 @@ class HomePage extends React.Component {
             </Grid.Column>
             <Grid.Column>
                 <Grid.Column>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident,
-                  sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p>Help a child forever by signing up</p>
+                  <p>
+                    Whether you are an individual or a company, we have opportunities for you to make an impact.
+                    You can start by donation or enrolling our adopt a child program.
+                  </p>
+                  <p>Help a child by signing up</p>
                   <Segment><SignupForm name="Sign up and Donate"/></Segment>
                 </Grid.Column>
             </Grid.Column>
